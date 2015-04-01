c7cf129b438aa941ca5aefa3c6957502e3b3430f
Katie Kelleher <kt.kllhr@gmail.com>
2015-04-01 13:55:18 -0700

    Added Username form to sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401011056) do
+ActiveRecord::Schema.define(version: 20150401204746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150401011056) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "username"
   end
 
   add_foreign_key "comments", "articles"
